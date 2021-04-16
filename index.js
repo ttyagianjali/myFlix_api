1c001b8c607f865c057bb8c8fd4e96221b628121
Anjali Tyagi <anjalityagi988@gmail.com>
2021-04-16 14:16:59 -0400

    connect the database on my local computer to mongodb atlas.

--- index.js
+++ index.js
@@ -35,7 +35,12 @@ app.use(
   })
 );
 
-mongoose.connect("mongodb://localhost:27017/movieManiaDb", {
+// mongoose.connect("mongodb://localhost:27017/movieManiaDb", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect("process.env.CONNECTION_URI", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
