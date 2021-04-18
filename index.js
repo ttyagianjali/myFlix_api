ce247fd7441eb8a24ee3690438ee2b805cb38eb6
Anjali Tyagi <anjalityagi988@gmail.com>
2021-04-18 07:57:10 -0400

    correction

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/", (req, res) => {
 app.get(
   "/movies",
   // passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+  function(req, res) {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
