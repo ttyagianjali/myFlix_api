030c80ae03c43442868f41a3ba8bec22d87192e7
Anjali Tyagi <anjalityagi988@gmail.com>
2021-04-23 04:32:21 -0400

    changed CORS configuration

--- index.js
+++ index.js
@@ -16,8 +16,27 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-const cors = require("cors");
-app.use(cors());
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // mongoose.connect("mongodb://localhost:27017/movieManiaDb", {
 //   useNewUrlParser: true,
