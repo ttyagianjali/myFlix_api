c2d4fd2ffb8455da04322425b1affed03ae596a9
Anjali Tyagi <anjalityagi988@gmail.com>
2021-06-15 16:14:29 -0400

    added get request for user end point

--- index.js
+++ index.js
@@ -114,6 +114,30 @@ app.get(
   }
 );
 
+// get information oof a user by username
+app.get(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOne({ "Userame": req.params.Username })
+      .then((user) => {
+        console.log(user);
+        res.status(200).json({
+          Username: user.Username,
+          Birthday: user.Birthday,
+          Email: user.Email,
+          Password: user.Password,
+          FavoriteMovies: user.FavoriteMovies
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
+
+
 //add a user
 /* We’ll expect JSON in this format
 {
