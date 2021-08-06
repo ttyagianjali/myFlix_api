612a08b6dc2a2f58b020387b0b700c6b3b42e28f
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-06 10:34:15 -0400

    changed cors configuration

--- index.js
+++ index.js
@@ -17,24 +17,7 @@ const passport = require("passport");
 require("./passport");
 
 const cors = require('cors');
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://testsite.com",
-  "http://localhost:1234",
-  "https://my-flix-007.netlify.app",
-  "http://localhost:4200",
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // mongoose.connect("mongodb://localhost:27017/movieManiaDb", {
 //   useNewUrlParser: true,
