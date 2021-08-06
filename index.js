6d99d0615a0f277a8c71af52bdef38d72f023af7
Anjali Tyagi <anjalityagi988@gmail.com>
2021-08-06 10:47:14 -0400

    update

--- index.js
+++ index.js
@@ -19,6 +19,26 @@ require("./passport");
 const cors = require('cors');
 app.use(cors());
 
+(function () {
+  var cors_api_host = "my-flix-007.herokuapp.com";
+  var cors_api_url = "https://" + cors_api_host + "/";
+  var slice = [].slice;
+  var origin = window.location.protocol + "//" + window.location.host;
+  var open = XMLHttpRequest.prototype.open;
+  XMLHttpRequest.prototype.open = function () {
+    var args = slice.call(arguments);
+    var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+    if (
+      targetOrigin &&
+      targetOrigin[0].toLowerCase() !== origin &&
+      targetOrigin[1] !== cors_api_host
+    ) {
+      args[1] = cors_api_url + args[1];
+    }
+    return open.apply(this, args);
+  };
+})();
+
 // mongoose.connect("mongodb://localhost:27017/movieManiaDb", {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
